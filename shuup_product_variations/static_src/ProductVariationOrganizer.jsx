ab63715e1d80928a00099a630d3d6864470170f0
Christian Hess <christianhess.rlz@gmail.com>
2021-05-03 17:24:00 -0300

    Fix license headers

--- shuup_product_variations/static_src/ProductVariationOrganizer.jsx
+++ shuup_product_variations/static_src/ProductVariationOrganizer.jsx
@@ -1,10 +1,11 @@
 /**
  * This file is part of Shuup.
  *
- * Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+ * Copyright (c) 2012-2021, Shuup Commerce Inc. All rights reserved.
  *
- * This source code is licensed under the OSL-3.0 license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the Shuup Commerce Inc -
+ * SELF HOSTED SOFTWARE LICENSE AGREEMENT executed by Shuup Commerce Inc, DBA as SHUUP®
+ * and the Licensee.
  */
 import React from 'react';
 import VariationOrganizer from './VariationOrganizer';
--- shuup_product_variations_tests/settings.py
+++ shuup_product_variations_tests/settings.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # This file is part of Shuup.
 #
-# Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+# Copyright (c) 2012-2021, Shuup Commerce Inc. All rights reserved.
 #
-# This source code is licensed under the OSL-3.0 license found in the
-# LICENSE file in the root directory of this source tree.
+# This source code is licensed under the Shuup Commerce Inc -
+# SELF HOSTED SOFTWARE LICENSE AGREEMENT executed by Shuup Commerce Inc, DBA as SHUUP®
+# and the Licensee.
 from shuup_workbench.settings.utils import get_disabled_migrations
 from shuup_workbench.test_settings import *  # noqa
 
